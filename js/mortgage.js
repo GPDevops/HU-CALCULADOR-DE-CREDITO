44cc4f38879c4d35601d2f720fee57ba755c664e
GPDEVOPS <gchalo17@gmail.com>
2025-09-24 21:26:49 -0500

    cambios realizados en js y html files

---
+++ js/mortgage.js
@@ -0,0 +1,23 @@
+//Definir los valores de entrada
+function calculateMortage() {
+    let valorTotal = document.forms["fmortgage"]["valorTotal"].value;
+    //alert("El valor Total es: " + valorTotal)
+    let cuotaInicial = document.forms["fmortgage"]["cuotaInicial"].value;
+    let tasaInteres = document.forms["fmortgage"]["tasaInteres"].value;
+    let plazo = document.forms["fmortgage"]["plazo"].value;
+    //alert("El valor Total es: " + valorTotal + " La cuota inicial es: " + cuotaInicial + " La tasa de interes: " + tasaInteres + " El plazo es: " + plazo)
+//Definir los valores de salida
+    const mortgage = {
+        totalPrestamo: 0,
+        totalIntereses: 0,
+        cuotaMensual: 0
+    };
+//Calculos
+const monthsOfYear = 12;
+mortgage.totalPrestamo = valorTotal - cuotaInicial;
+//alert("La diferencia entre el valor total del inmueble y la cuota de entrada es: " + mortgage.totalPrestamo)
+mortgage.totalIntereses = mortgage.totalPrestamo * (tasaInteres/100);
+//alert("El total de interes es: " + mortgage.totalIntereses)
+mortgage.cuotaMensual = (mortgage.totalPrestamo + mortgage.totalIntereses) / (plazo*monthsOfYear);
+alert("La cuota mensual es: " + mortgage.cuotaMensual)
+}
