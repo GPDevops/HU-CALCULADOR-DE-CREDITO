7160174c3335ddcddb9d7ffe345083eed9d2b1d5
GPDEVOPS <gchalo17@gmail.com>
2025-09-30 19:49:28 -0500

    new changes

--- js/mortgage.js
+++ js/mortgage.js
@@ -32,10 +32,14 @@ function outputMortgage(datosFinales) {
     document.getElementById("valorCuota").innerHTML = valueToDollar(datosFinales.cuotaMensual);
     var totalPrestamoPorcentaje = 0;
     totalPrestamoPorcentaje = (datosFinales.totalPrestamo *100 / datosFinales.costoTotalInmueble);
-    alert("El porcentaje del prestamo es: " + totalPrestamoPorcentaje);
+    //alert("El porcentaje del prestamo es: " + totalPrestamoPorcentaje);  //Debug
+    //Cambiar el color del monto del prestamo si es mayor al 90%
     //<output id="montoPrestamo" class="form-control alertaPorcentaje">0</output>  <---a modificar esta clase del index.html
     if (totalPrestamoPorcentaje > 90) {
-        document.getElementById("montoPrestamo").className += " alertaPorcentaje";
+        document.getElementById("montoPrestamo").className += " alertaPorcentaje"; //Agregar clase para resaltar el valor en rojo
+    }else {
+        //document.getElementById("montoPrestamo").classList.remove("alertaPorcentaje"); //Remover clase
+        document.getElementById("montoPrestamo").className = "form-control"; //Reestablecer clase original
     }
 }
 //Reiniciar formulario
