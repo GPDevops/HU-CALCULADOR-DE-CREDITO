c10dd126aba6d628f2a310348a680494c1655fcd
GPDEVOPS <gchalo17@gmail.com>
2025-10-02 20:37:35 -0500

    final con errores de duplicados

--- js/form.js
+++ js/form.js
@@ -36,9 +36,10 @@ function processContactForm(e) {
     persona.correo = document.forms["formDatos"]["correo"].value;
     persona.pais = document.forms["formDatos"]["pais"].value;
     persona.ciudad = document.forms["formDatos"]["ciudad"].value;
-
-    //persona.id = personasArray.length;   //Asignar un ID unico a cada objeto
-    persona.id = allStorage().length;   //Asignar un ID unico a cada objeto
+    if (persona.id <= 0) {
+        //persona.id = allStorage().length;   //Asignar un ID unico a cada objeto  //persona.id = personasArray.length; 
+        persona.id = new Date().valueOf(); //Asignar un ID unico a cada objeto por otro metodo
+    }
 
     //Convertir el objeto a formato JSON
     let personaJson = JSON.stringify(persona);
@@ -50,13 +51,21 @@ function processContactForm(e) {
     e.preventDefault();
     //alert("Datos ingresados con exito: " + personasArray.toString());
     alert("Datos ingresados con exito: ");
+    listarcontactos();
+    resetform();
 }
+
+function resetform(){
+    document.forms["formDatos"].reset();
+    persona.id = 0;
+}
+
 //Crear tabla dinamica
 
 function listarcontactos() {
     let dinamicTable = "";
     //cabecera de la tabla
-    dinamicTable += "<table class='table table-striped'>";
+    dinamicTable += "<table class='table'>";
     dinamicTable += "<tr>";
     dinamicTable += "<th>Id</th>";
     dinamicTable += "<th>Nombres</th>";
@@ -72,16 +81,19 @@ function listarcontactos() {
     personasGuardadas = allStorage();
     for (let i = 0; i < personasGuardadas.length; i++) {
         dinamicTable += "<tr>";
-            let personaobjeto = JSON.parse(personasGuardadas[i]);
-            dinamicTable += "<td>" + personaobjeto.id + "</td>";
-            dinamicTable += "<td>" + personaobjeto.nombres + "</td>";
-            dinamicTable += "<td>" + personaobjeto.apellidos + "</td>";
-            dinamicTable += "<td>" + personaobjeto.numeroTelefono + "</td>";
-            dinamicTable += "<td>" + personaobjeto.correo + "</td>";
-            dinamicTable += "<td>" + personaobjeto.pais + "</td>";
-            dinamicTable += "<td>" + personaobjeto.ciudad + "</td>";
-            dinamicTable += "<td>" + '<a href="./detalles.html?id='+ personaobjeto.id +'">Ver</a>' + "</td>";
+        let personaobjeto = JSON.parse(personasGuardadas[i]);
+        dinamicTable += "<td>" + personaobjeto.id + "</td>";
+        dinamicTable += "<td>" + personaobjeto.nombres + "</td>";
+        dinamicTable += "<td>" + personaobjeto.apellidos + "</td>";
+        dinamicTable += "<td>" + personaobjeto.numeroTelefono + "</td>";
+        dinamicTable += "<td>" + personaobjeto.correo + "</td>";
+        dinamicTable += "<td>" + personaobjeto.pais + "</td>";
+        dinamicTable += "<td>" + personaobjeto.ciudad + "</td>";
+        dinamicTable += "<td>" + '<a href="./detalles.html?id=' + personaobjeto.id + '">Ver</a>' + "</td>";
+        dinamicTable += "<td>" + '<a href="javascript:editarContacto(' + personaobjeto.id + ');">Editar</a>' + "</td>";
+        dinamicTable += "<td>" + '<a href="javascript:eliminarContacto(' + personaobjeto.id + ');">Eliminar</a>' + "</td>";
         dinamicTable += "</tr>";
+        dinamicTable += "<tr>";
     }
     dinamicTable += "</table>";
     document.getElementById("tablecontact").innerHTML = dinamicTable;
@@ -98,11 +110,11 @@ function allStorage() {
     return values;
 }
 
-//Mostrar detalles del contacto
+//Pinta en la vista de detalles la informacion del cotnacto seleccionado
 function verDetalles() {
     let contactoId = obtenerParametroUrl();
     let contacto = localStorage.getItem(contactoId);
-    if (contacto.length) {
+    if (contacto.length > 0) {
         let personaObjeto = JSON.parse(contacto);
         document.getElementById("nombres").innerText = personaObjeto.nombres;
         document.getElementById("apellidos").innerText = personaObjeto.apellidos;
@@ -113,16 +125,46 @@ function verDetalles() {
     }
 }
 
+//busca un contacto y lo pinta en el formulario para permitir modificarlo
+
+function editarContacto(id) {
+    let contacto = localStorage.getItem(id);
+    if (contacto.length > 0) {
+        let personaObjeto = JSON.parse(contacto);
+        document.getElementById("nombres").value = personaObjeto.nombres;
+        document.getElementById("apellidos").value = personaObjeto.apellidos;
+        document.getElementById("numeroTelefono").value = personaObjeto.numeroTelefono;
+        document.getElementById("correo").value = personaObjeto.correo;
+        document.getElementById("pais").value = personaObjeto.pais;
+        document.getElementById("ciudad").value = personaObjeto.ciudad;
+        persona.id = id;
+    }
+    listarcontactos();
+}
+
+//elimina un contacto del localstorage 
+function eliminarContacto(id) {
+    let contacto = localStorage.getItem(id);
+    if (contacto.length > 0) {
+        localStorage.removeItem(id);
+        alert("Contacto eliminado");
+    }
+    listarcontactos();
+}
+
 function obtenerParametroUrl() {
     let url = window.location.href;
     let paramString = url.split('?')[1];
     let queryString = new URLSearchParams(paramString);
     let parameterId = 0;
     for (let pair of queryString.entries()) {
-        //console.log("Key: " + pair[0]);
-        //console.log("Value: " + pair[1]);
-        parameterId = pair[1];
-        
+        console.log("Key: " + pair[0]);
+        console.log("Value: " + pair[1]);
+        parameterId = Number(pair[1]);
+
     }
     return parameterId;
 }
+
+
+
